e9dd55f4b516d99dcc31114f7f56ece9a9cf30b8
José Antônio Sousa da Silva <tony@arpiatecnologia.com.br>
2022-03-27 18:51:13 -0300

    feat: implements import csv files

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+
+import { ImportCategoryUseCase } from './ImportCategoryUseCase';
+
+class ImportCategoryController {
+  constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { file } = request;
+    this.importCategoryUseCase.execute(file as Express.Multer.File);
+
+    return response.json({ message: 'Importação realizada com sucesso!' });
+  }
+}
+
+export { ImportCategoryController };
